7bb23489efff13282871b4f119622bd883f6dd74
Martin Stofko <martinstofko114@gmail.com>
2022-08-24 22:10:12 -0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Martin Stofko on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
