db27b436f9cf07c77037613ee393352b0839a608
Martin Stofko <martinstofko114@gmail.com>
2022-08-30 21:56:08 -0700

    Finished work on animations.

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,6 +13,10 @@ struct OnboardingView: View {
     
     @State private var isAnimating = false
     @State private var buttonOffset: CGFloat = 0
+    @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle = "Share."
+    
     private let buttonWidth = UIScreen.main.bounds.width - 80
     
     var body: some View {
@@ -24,10 +28,12 @@ struct OnboardingView: View {
                 
                 // MARK: TEXT
                 VStack {
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)
                     
                     Text("It's not how much we give but\nhow much love we put into giving.")
                         .font(.title3)
@@ -45,11 +51,47 @@ struct OnboardingView: View {
                 // MARK: ILLUSTRATION
                 ZStack {
                     CircleGroupView(shapeColor: .white, shapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)
+                        .blur(radius: abs(imageOffset.width / 5))
+                        .animation(.easeOut(duration: 1.25), value: imageOffset)
+                    
                     Image("character-1")
                         .resizable()
                         .scaledToFit()
                         .opacity(isAnimating ? 1 : 0)
                         .animation(.easeOut(duration: 0.75), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0)
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged({ gesture in
+                                    // using absolute so we don't have to worry about negative translation
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Give."
+                                        }
+                                    }
+                                })
+                                .onEnded({ _ in
+                                    imageOffset = .zero
+                                    withAnimation(.linear(duration: 0.25).delay(0.5)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                })
+                        )
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                }
+                .overlay(alignment: .bottom) {
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(1.5), value: isAnimating)
+                        .opacity(indicatorOpacity)
                 }
                 
                 // MARK: CUSTOM BUTTON
@@ -113,9 +155,8 @@ struct OnboardingView: View {
                 .animation(.easeOut(duration: 1), value: isAnimating)
             }
         }
-        .onAppear {
-            isAnimating = true
-        }
+        .onAppear { isAnimating = true }
+        .preferredColorScheme(.dark)
     }
 }
 
